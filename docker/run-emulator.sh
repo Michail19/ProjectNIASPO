c128193e219c978b5cbf94b9c724a4ed3be9d622
Michail <62212858+Michail19@users.noreply.github.com>
2024-11-08 23:54:50 +0300

    Updates for starting emulator

--- docker/run-emulator.sh
+++ docker/run-emulator.sh
@@ -8,7 +8,7 @@ export DISPLAY=:0
 x11vnc -display :0 -forever -shared -rfbport 5900 &
 
 # Запуск Android эмулятора с логированием
-$ANDROID_HOME/emulator/emulator -avd test_avd -no-window -no-audio -gpu host -accel on -qemu -enable-kvm &
+$ANDROID_HOME/emulator/emulator -avd test_avd -no-window -no-audio -gpu swiftshader_indirect -accel on -qemu -enable-kvm &
 
 # Пауза для запуска эмулятора
 adb wait-for-device
--- docker/start-server.sh
+++ docker/start-server.sh
@@ -4,10 +4,10 @@
 export TZ=Europe/London
 
 # Запуск Xvfb
-Xvfb :99 -screen 0 1024x768x16 &
+Xvfb :0 -screen 0 1024x768x16 &
 
 # Запуск x11vnc
-x11vnc -display :99 -forever -shared -rfbport 5900 &
+x11vnc -display :0 -forever -shared -rfbport 5900 &
 
 # Запуск noVNC
 /noVNC/utils/launch.sh --vnc localhost:5900 --listen 6080
