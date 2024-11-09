015c14e747d2073c0531caa181771a88f22a4e74
Michail <62212858+Michail19@users.noreply.github.com>
2024-11-09 12:58:33 +0300

    Emulator test

--- docker/run-emulator.sh
+++ docker/run-emulator.sh
@@ -3,15 +3,21 @@
 # Запуск виртуального дисплея Xvfb
 Xvfb :0 -screen 0 1080x1920x24 &
 export DISPLAY=:0
+echo "Xvfb запущен на дисплее $DISPLAY"
 
 # Запуск VNC-сервера x11vnc
 x11vnc -display :0 -forever -shared -rfbport 5900 &
+echo "VNC-сервер запущен на дисплее $DISPLAY"
 
 # Запуск Android эмулятора с логированием
 $ANDROID_HOME/emulator/emulator -avd test_avd -no-window -no-audio -gpu swiftshader_indirect -accel on -qemu -enable-kvm &
+echo "Эмулятор запущен"
 
-# Пауза для запуска эмулятора
-adb wait-for-device
+# Даем эмулятору время на загрузку
+sleep 30
+
+# Проверка состояния эмулятора
+adb wait-for-device && echo "Эмулятор доступен через adb" || echo "Эмулятор не доступен через adb"
 
 # Оставляем контейнер активным, пока эмулятор работает
 wait $!
--- docker/start-server.sh
+++ docker/start-server.sh
@@ -4,10 +4,10 @@
 export TZ=Europe/London
 
 # Запуск Xvfb
-Xvfb :0 -screen 0 1080x1920x24 &
+Xvfb :1 -screen 0 1080x1920x24 &
 
 # Запуск x11vnc
-x11vnc -display :0 -forever -shared -rfbport 5900 &
+x11vnc -display :1 -forever -shared -rfbport 5900 &
 
 # Запуск noVNC
 /noVNC/utils/launch.sh --vnc localhost:5900 --listen 6080
