caf00f8e6c8aa6714641820577a8f350b8b56883
Michail <62212858+Michail19@users.noreply.github.com>
2024-11-09 17:48:44 +0300

    Update display settings

--- docker/start-server.sh
+++ docker/start-server.sh
@@ -4,10 +4,10 @@
 export TZ=Europe/London
 
 # Запуск Xvfb
-Xvfb :1 -screen 0 1080x1920x24 &
+Xvfb :1 -screen 1 1080x1920x24 &
 
 # Запуск x11vnc
-x11vnc -display :1 -forever -shared -rfbport 5900 &
+x11vnc -display :1 -forever -shared -rfbport 5901 &
 
 # Запуск noVNC
-/noVNC/utils/launch.sh --vnc localhost:5900 --listen 6080
+/noVNC/utils/launch.sh --vnc localhost:5901 --listen 6080
