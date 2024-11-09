6cfdb03a8a0e8a0163b8002f803901f743f82920
Michail <62212858+Michail19@users.noreply.github.com>
2024-11-10 00:03:05 +0300

    Some updates

--- docker/run-emulator.sh
+++ docker/run-emulator.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
 # Задаем порт ADB
-export ANDROID_ADB_SERVER_PORT=5038
+export ANDROID_ADB_SERVER_PORT=5037
 
 # Запускаем ADB сервер на новом порту
 adb -P $ANDROID_ADB_SERVER_PORT start-server
 
 fluxbox &
 
+export DISPLAY=:0 xlsclients
+xhost +local:root
+
 # Запуск виртуального дисплея Xvfb на :0
 Xvfb :0 -screen 0 1280x720x16 &
-export DISPLAY=:0 xlsclients
 
 # Запуск VNC-сервера для дисплея
 x11vnc -display :0 -forever -shared -rfbport 5900 -noxdamage -ncache 10 &
 
 # Запуск Android эмулятора
-$ANDROID_HOME/emulator/emulator -avd test_avd -port 5554 -no-window -no-audio -gpu host -accel on -qemu -enable-kvm > emulator.log 2>&1 &
+$ANDROID_HOME/emulator/emulator -avd test_avd -port 5554 -no-window -no-audio -gpu host -accel on -qemu -enable-kvm &
 
 # Ожидание загрузки эмулятора
 sleep 30
