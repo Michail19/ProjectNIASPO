566b8d9fdcf2bd1290e27c6022a27cc07f2d21c5
Michail <62212858+Michail19@users.noreply.github.com>
2024-11-12 00:13:26 +0300

    Гзвфеу ыукмук

--- docker/run-emulator.sh
+++ docker/run-emulator.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Запуск виртуального дисплея
-Xvfb :0 -screen 0 1280x800x16 &
+# Запуск adb-сервера
+adb start-server
 
 # Запуск эмулятора Android
 $ANDROID_SDK_ROOT/emulator/emulator -avd test -no-window -no-audio -gpu swiftshader_indirect -accel on -qemu -enable-kvm
--- docker/start-server.sh
+++ docker/start-server.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
+# Запуск виртуального дисплея
+Xvfb :0 -screen 0 1280x800x16 &
+
+# Запуск оконного менеджера
+fluxbox &
+
 # Запуск x11vnc для подключения к X-сессии
 x11vnc -display :0 -noshm -nopw -forever &
 
-# Запуск noVNC через скрипт launch.sh, если он доступен
+# Запуск noVNC через launch.sh или websockify
 if [ -f /usr/share/novnc/utils/launch.sh ]; then
     /usr/share/novnc/utils/launch.sh --vnc localhost:5900 --listen 6080
 else
-    # В случае отсутствия launch.sh используем websockify напрямую
     websockify --web /usr/share/novnc 6080 localhost:5900
 fi
