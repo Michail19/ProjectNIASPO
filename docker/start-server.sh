d4f73b0020a8c7ee2954246e25704e0d3447b7ed
Michail <62212858+Michail19@users.noreply.github.com>
2024-11-11 23:09:52 +0300

    Updates novnc

--- docker/start-server.sh
+++ docker/start-server.sh
@@ -3,5 +3,10 @@
 # Запуск x11vnc для подключения к X-сессии
 x11vnc -display :0 -nopw -forever &
 
-# Запуск noVNC сервера
-novnc --vnc localhost:5900 --listen 6080
+# Запуск noVNC через скрипт launch.sh, если он доступен
+if [ -f /usr/share/novnc/utils/launch.sh ]; then
+    /usr/share/novnc/utils/launch.sh --vnc localhost:5900 --listen 6080
+else
+    # В случае отсутствия launch.sh используем websockify напрямую
+    websockify --web /usr/share/novnc 6080 localhost:5900
+fi
