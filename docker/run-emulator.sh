439a285ae6da11b7cf36f49907bdc04cb19e072c
Michail <62212858+Michail19@users.noreply.github.com>
2024-11-11 19:58:04 +0300

    Some changes

--- docker/run-emulator.sh
+++ docker/run-emulator.sh
@@ -6,17 +6,17 @@ export ANDROID_ADB_SERVER_PORT=5037
 # Запускаем ADB сервер на новом порту
 adb -P $ANDROID_ADB_SERVER_PORT start-server
 
-# Запуск fluxbox для оконного управления
-fluxbox &
+# Запуск openbox для оконного управления
+openbox &
 
 # Запуск виртуального дисплея Xvfb на :0
-Xvfb :0 -screen 0 1280x720x16 &
+Xvfb :0 -screen 0 1280x1024x24 &
 
 # Запуск VNC-сервера для дисплея
 x11vnc -display :0 -forever -shared -rfbport 5900 -noxdamage -ncache 10 &
 
 # Запуск Android эмулятора
-$ANDROID_HOME/emulator/emulator -avd test_avd -port 5554 -no-window -no-audio -gpu host -accel on -qemu -enable-kvm -partition-size 2048 -memory 1024 &
+$ANDROID_HOME/emulator/emulator -avd test_avd -port 5554 -no-window -no-audio -gpu host -accel on -qemu -m 2048 &> /root/emulator.log &
 
 # Ожидание загрузки эмулятора
 sleep 30
