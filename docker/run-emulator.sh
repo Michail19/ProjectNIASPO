fe4fe3ccb1b61761610a970d2b9ffc349eaf888c
Michail <62212858+Michail19@users.noreply.github.com>
2024-11-14 20:54:37 +0300

    Entrypoint

--- docker/run-emulator.sh
+++ docker/run-emulator.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Ensure the correct permissions on /tmp/.X11-unix
-sudo chown root:root /tmp/.X11-unix
+chmod -R 1777 /tmp/.X11-unix
+chown root:root /tmp/.X11-unix
 
 # Запуск adb-сервера
 adb start-server
@@ -9,3 +10,9 @@ adb start-server
 # Запуск эмулятора Android
 $ANDROID_SDK_ROOT/emulator/emulator -avd test -no-window -no-audio -port 5554 -gpu host -accel on -qemu -enable-kvm
 
+# Ждем некоторое время для старта эмулятора
+sleep 30
+
+# Переключаем ADB на TCP и подключаемся
+adb -s emulator-5554 tcpip 5555
+adb connect localhost:5555
--- docker/start-server.sh
+++ docker/start-server.sh
@@ -9,8 +9,8 @@ x11vnc -display :0 -noshm -nopw -forever &
 sleep 2  # Даем время для запуска x11vnc
 
 # Проверка наличия launch.sh и запуск noVNC
-if [ -f /usr/share/novnc/utils/launch.sh ]; then
-    /usr/share/novnc/utils/launch.sh --vnc localhost:5900 --listen 6080 &
+if [ -f /usr/share/novnc/noVNC-1.2.0/utils/launch.sh ]; then
+    /usr/share/novnc/noVNC-1.2.0/utils/launch.sh --vnc localhost:5900 --listen 6080 &
 else
     # Запуск websockify, если launch.sh недоступен
     websockify --web /usr/share/novnc 6080 localhost:5900 &
