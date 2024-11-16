8c4a3412d03904a715d6c7c08d4ff1ae3342328d
Michail <62212858+Michail19@users.noreply.github.com>
2024-11-16 21:33:09 +0300

    Revert changes

--- docker/run-emulator.sh
+++ docker/run-emulator.sh
@@ -3,7 +3,7 @@
 export DISPLAY=:99
 export XDG_RUNTIME_DIR=/tmp
 export LIBGL_ALWAYS_SOFTWARE=1
-export ADB_SERVER_PORT=5037
+export ADB_SERVER_PORT=5038
 
 # Создаем директорию .android, если она не существует
 mkdir -p /root/.android
@@ -24,27 +24,14 @@ for i in {1..10}; do
 done
 
 # Запускаем эмулятор Android
-$ANDROID_SDK_ROOT/emulator/emulator -avd test -no-audio -port 5554 -no-skin -gpu host -accel on -qemu -enable-kvm &
-
-sleep 15
+$ANDROID_SDK_ROOT/emulator/emulator -avd test -no-audio -port 5554 -gpu host -accel on -qemu -enable-kvm &
 
 # Запускаем ADB сервер
-export ADB_SERVER_SOCKET=/tmp/adb/adb.sock
-adb kill-server  # Убедитесь, что сервер перезапущен
-adb -H localhost -P 5037 devices
+adb start-server
 
-echo 0 >/tmp/adb/adb.log
-chmod 777 /tmp/adb/adb.sock
-chmod 777 /tmp/adb/adb.log
-chown root:root /tmp/adb/adb.sock
-chown root:root /tmp/adb/adb.log
-# adb start-server
-
-unset ADB_SERVER_SOCKET
 # Ждём, пока adb станет доступен
 for i in {1..10}; do
-    # if adb get-state >/dev/null 2>&1; then
-    if adb get-state >>/tmp/adb/adb.log 2>&1; then
+    if adb get-state >/dev/null 2>&1; then
         echo "ADB is responding."
         break
     fi
@@ -52,21 +39,15 @@ for i in {1..10}; do
     sleep 2
 done
 
-ls -la /tmp/adb
-
 # Ждем завершения загрузки эмулятора
 boot_completed=""
 while [ "$boot_completed" != "1" ]; do
-    # boot_completed=$(adb shell getprop sys.boot_completed 2>/dev/null | tr -d '\r')
-    boot_completed=$(adb shell getprop sys.boot_completed 2>>/tmp/adb/adb.log | tr -d '\r')
+    boot_completed=$(adb shell getprop sys.boot_completed 2>/dev/null | tr -d '\r')
     echo "Waiting for emulator to boot..."
     sleep 5
 done
 
 echo "Emulator boot completed."
 
-export ADB_SERVER_SOCKET=/tmp/adb/adb.sock
-
 # Оставляем контейнер активным
-# tail -f /dev/null
-tail -f /tmp/adb/adb.log
+tail -f /dev/null
