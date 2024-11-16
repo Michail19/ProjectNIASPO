9d82e9f8aa3d1132dbccd38e1da21c6f42e3294f
Michail <62212858+Michail19@users.noreply.github.com>
2024-11-17 01:04:55 +0300

    Some updates

--- docker/run-emulator.sh
+++ docker/run-emulator.sh
@@ -3,7 +3,7 @@
 export DISPLAY=:99
 export XDG_RUNTIME_DIR=/tmp
 export LIBGL_ALWAYS_SOFTWARE=1
-export ADB_SERVER_PORT=5038
+export ADB_SERVER_PORT=5037
 
 # Создаем директорию .android, если она не существует
 mkdir -p /root/.android
@@ -14,6 +14,7 @@ chmod -R 1777 /tmp/.X11-unix
 chown root:root /tmp/.X11-unix
 
 # Запускаем ADB сервер
+export ADB_SERVER_SOCKET=/adb-shared/adb.sock
 adb -a -P 5037 nodaemon server &
 
 # Ждем запуска Xvfb
@@ -27,13 +28,15 @@ for i in {1..10}; do
 done
 
 # Запускаем эмулятор Android
-$ANDROID_SDK_ROOT/emulator/emulator -avd test -no-audio -port 5554 -gpu host -accel on -qemu -enable-kvm &
+$ANDROID_SDK_ROOT/emulator/emulator -avd test -no-audio -port 5554 -no-snapshot -writable-system -gpu host -accel on -qemu -enable-kvm &
 
 sleep 10
 
+unset ADB_SERVER_SOCKET
 # Ждём, пока adb станет доступен
 for i in {1..10}; do
-    if adb get-state >/dev/null 2>&1; then
+#    if adb get-state >/dev/null 2>&1; then
+    if adb get-state >/adb-shared/adb.log 2>&1; then
         echo "ADB is responding."
         break
     fi
@@ -44,12 +47,15 @@ done
 # Ждем завершения загрузки эмулятора
 boot_completed=""
 while [ "$boot_completed" != "1" ]; do
-    boot_completed=$(adb shell getprop sys.boot_completed 2>/dev/null | tr -d '\r')
+#    boot_completed=$(adb shell getprop sys.boot_completed 2>/dev/null | tr -d '\r')
+    boot_completed=$(adb shell getprop sys.boot_completed 2>/adb-shared/adb.log | tr -d '\r')
     echo "Waiting for emulator to boot..."
     sleep 5
 done
 
 echo "Emulator boot completed."
+export ADB_SERVER_SOCKET=/adb-shared/adb.sock
 
 # Оставляем контейнер активным
-tail -f /dev/null
+# tail -f /dev/null
+tail -f /adb-shared/adb.log
