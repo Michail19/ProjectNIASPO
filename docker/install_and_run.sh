af10c408701971f4c1a563bafb26484d8f070c98
Michail <62212858+Michail19@users.noreply.github.com>
2024-11-18 16:26:30 +0300

    New Try

--- docker/install_and_run.sh
+++ docker/install_and_run.sh
@@ -4,10 +4,10 @@
 SERVICE_NAME=docker-gradle-1
 
 # Сборка APK
-docker exec -it "$SERVICE_NAME" bash -c "/app/gradlew -p /app assembleDebug"
+/app/gradlew -p /app assembleDebug
 
 # Установка APK на эмулятор
-docker exec -it "$SERVICE_NAME" bash -c "adb -s emulator-5554 install -r /app/app/build/outputs/apk/debug/app-debug.apk"
+adb -s emulator-5554 install -r /app/app/build/outputs/apk/debug/app-debug.apk
 
 # Запуск приложения
-docker exec -it "$SERVICE_NAME" bash -c "adb -s emulator-5554 shell monkey -p com.me.projectniaspo -c android.intent.category.LAUNCHER 1"
+adb -s emulator-5554 shell monkey -p com.me.projectniaspo -c android.intent.category.LAUNCHER 1
